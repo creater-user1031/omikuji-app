91d496faa55b5710b9aa315f1cc80969c4907c6c
Harutuki <hafutuki@gmail.com>
2026-04-19 19:59:40 +0900

    おみくじデータの作成

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const fortune = document.getElementById("fortune");
 const wishInput = document.getElementById("wishInput");
 
 button.addEventListener("click", function () {
-    const wish = wishInput.value;
 
-    fortune.textContent = "あなたの願い：「" + wish + "」";
+    const wish = wishInput.value;
 
     if (wish === "") {
         fortune.textContent = "願い事が記載されていないため、おみくじを引くことができませんでした。";
         return;
     }
+
+    fortune.textContent = "あなたの願い：「" + wish + "」";
 })
